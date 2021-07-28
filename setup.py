6f691650fd3ef900560294cbd88b9192feba9b74
tk <tk@renard.ninja>
2021-07-28 12:41:32 +0000

    update circle ci config

--- setup.py
+++ setup.py
@@ -7,86 +7,84 @@ import re
 
 from setuptools import setup, find_packages
 
-RE_REQUIREMENT = re.compile(r'^\s*-r\s*(?P<filename>.*)$')
-RE_BADGE = re.compile(r'^\[\!\[(?P<text>[^\]]+)\]\[(?P<badge>[^\]]+)\]\]\[(?P<target>[^\]]+)\]$', re.M)
+RE_REQUIREMENT = re.compile(r"^\s*-r\s*(?P<filename>.*)$")
+RE_BADGE = re.compile(
+    r"^\[\!\[(?P<text>[^\]]+)\]\[(?P<badge>[^\]]+)\]\]\[(?P<target>[^\]]+)\]$", re.M
+)
 
-BADGES_TO_KEEP = ['gitter-badge', 'readthedocs-badge']
+BADGES_TO_KEEP = ["gitter-badge", "readthedocs-badge"]
 
 
 def md(filename):
-    '''
+    """
     Load .md (markdown) file and sanitize it for PyPI.
     Remove unsupported github tags:
      - code-block directive
      - travis ci build badges
-    '''
+    """
     content = io.open(filename).read()
 
     for match in RE_BADGE.finditer(content):
-        if match.group('badge') not in BADGES_TO_KEEP:
-            content = content.replace(match.group(0), '')
+        if match.group("badge") not in BADGES_TO_KEEP:
+            content = content.replace(match.group(0), "")
     return content
 
 
-long_description = '\n'.join((
-    md('README.md'),
-    md('CHANGELOG.md'),
-    ''
-))
+long_description = "\n".join((md("README.md"), md("CHANGELOG.md"), ""))
 
 
 def pip(filename):
-    '''Parse pip requirement file and transform it to setuptools requirements'''
+    """Parse pip requirement file and transform it to setuptools requirements"""
     requirements = []
-    for line in open(os.path.join('requirements', filename)).readlines():
+    for line in open(os.path.join("requirements", filename)).readlines():
         match = RE_REQUIREMENT.match(line)
         if match:
-            requirements.extend(pip(match.group('filename')))
+            requirements.extend(pip(match.group("filename")))
         else:
             requirements.append(line)
     return requirements
 
 
-install_requires = pip('install.pip')
+install_requires = pip("install.pip")
 
 setup(
-    name='cada',
-    version='1.0.1.dev',
-    description='Search and consult CADA advices',
+    name="cada",
+    version="1.0.1.dev",
+    description="Search and consult CADA advices",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/etalab/cada',
-    author='Axel Haustant',
-    author_email='axel@data.gouv.fr',
+    long_description_content_type="text/markdown",
+    url="https://github.com/etalab/cada.data.gouv.fr",
+    author="Axel Haustant",
+    author_email="axel@data.gouv.fr",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=install_requires,
-    setup_requires=['setuptools>=38.6.0'],
-    python_requires='>=3.6.*',
+    setup_requires=["setuptools>=38.6.0"],
+    python_requires=">=3.6.*",
     extras_require={
-        'sentry': pip('sentry.pip'),
-        'test': pip('test.pip'),
-        'report': pip('report.pip'),
+        "sentry": pip("sentry.pip"),
+        "test": pip("test.pip"),
+        "report": pip("report.pip"),
     },
     entry_points={
-        'console_scripts': [
-            'cada = cada.commands:cli',
+        "console_scripts": [
+            "cada = cada.commands:cli",
         ]
     },
-    license='AGPLv3+',
-    keywords='cada',
+    license="AGPLv3+",
+    keywords="cada",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python',
-        'Environment :: Web Environment',
-        'Operating System :: OS Independent',
-        'Intended Audience :: Developers',
-        'Topic :: System :: Software Distribution',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: System :: Software Distribution",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
     ],
 )
